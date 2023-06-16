8c0ba0836514b09a935f2a3a10958a94be8a0d19
Wiznat <wyattmccabe@gmail.com>
2023-06-15 21:06:10 -0700

    Added voice generator

---
+++ VoiceGenerator.py
@@ -0,0 +1,70 @@
+from gradio_client import Client
+
+
+class VoiceGenerator:
+
+    def __init__(self, url):
+        self.client = Client(url)
+        self.url = url
+        self.input_prompt = ""
+        self.line_delimiter = "\\n"
+        self.emotion = "None"
+        self.custom_emotion = None
+        self.voice = "Random"
+        self.microphone_source = None
+        self.voice_chunks = 0
+        self.candidates = 1
+        self.seed = 42
+        self.samples = 2
+        self.iterations = 30
+        self.temperature = 1.0
+        self.diffusion_samplers = "DDIM"
+        self.pause_size = 8
+        self.cvvp_weight = 0
+        self.top_p = 0.8
+        self.diffusion_temperature = 1
+        self.length_penalty = 7
+        self.repetition_penalty = 7
+        self.conditioningfree_k = 2
+        self.experimental_flags = ["Conditioning-Free"]
+        self.use_original_latents_method_ar = False
+        self.use_original_latents_method_diffusion = False
+        self.api = "/generate"
+
+    def generateLines(self, lines, voice):
+        for line in lines:
+            self.generateLine(line, voice)
+
+        #downloadResult(script);
+
+    def generateLine(self, text, voice):
+        try:
+            result = self.client.predict(
+            text,
+            self.line_delimiter,
+            self.emotion,
+            self.custom_emotion,
+            voice,
+            self.microphone_source,
+            self.voice_chunks,
+            self.candidates,
+            self.seed,
+            self.samples,
+            self.iterations,
+            self.temperature,
+            self.diffusion_samplers,
+            self.pause_size,
+            self.cvvp_weight,
+            self.top_p,
+            self.diffusion_temperature,
+            self.length_penalty,
+            self.repetition_penalty,
+            self.conditioningfree_k,
+            self.experimental_flags,
+            self.use_original_latents_method_ar,
+            self.use_original_latents_method_diffusion,
+            api_name = self.api
+        )
+        except:
+            print("File Generated")
+
